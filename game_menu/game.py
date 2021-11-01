d94794ef15dafadf4d0338430966c76312ae3287
moura <m0ur4@protonmail.com>
2021-11-01 11:29:49 -0300

    add: Created smooth fade-in and fade-out.

--- game_menu/game.py
+++ game_menu/game.py
@@ -81,11 +81,12 @@ def main() -> None:
 
     pygame.init()
 
+    SCREEN_SIZE = (600, 400)
+
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((600, 400))
+    screen = pygame.display.set_mode(SCREEN_SIZE)
     screen_rect = screen.get_rect()
 
-    # Load images
     logo_icon_image = generate_image("moura_cat.png")
     logo_title_image = generate_image("logo_title.png")
     game_icon_image = generate_image("icon.png")
@@ -94,12 +95,18 @@ def main() -> None:
     settings_button_image = generate_image("settings_button_off.png")
     quit_button_image = generate_image("quit_button_off.png")
 
+    white_background = pygame.Surface(SCREEN_SIZE)
+    white_background = white_background.convert_alpha()
+
     logo_icon_rect = logo_icon_image.get_rect()
     logo_title_rect = logo_title_image.get_rect()
     game_title_rect = game_title_image.get_rect()
     play_button_rect = play_button_image.get_rect()
     settings_button_rect = settings_button_image.get_rect()
     quit_button_rect = quit_button_image.get_rect()
+
+    white_background_rect = white_background.get_rect()
+    white_background_rect.center = screen_rect.center
 
     setup_intro_view(screen_rect, logo_icon_rect, logo_title_rect)
     setup_main_menu_view(screen_rect, game_title_rect, play_button_rect,
@@ -120,8 +127,6 @@ def main() -> None:
         nonlocal quit_button_image
 
         mouse_pos = pygame.mouse.get_pos()
-        print(game_title_rect.bottom)
-        print(game_title_rect.top)
         if game_title_rect.bottom >= 120 \
            or game_title_rect.top <= screen_rect.top:
             yspeed *= -1
@@ -142,16 +147,49 @@ def main() -> None:
         else:
             quit_button_image = generate_image("quit_button_off.png")
 
+    alpha_value = 255
+    factor = 1
+
+    backwards = False
+    on_intro = True
+    fade_counter = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        screen.fill((170, 170, 170))
-        main_menu_view(screen, game_title_image, play_button_image,
-                       settings_button_image, quit_button_image,
-                       game_title_rect, play_button_rect,
-                       settings_button_rect, quit_button_rect)
-        animate_main_menu_view()
-        clock.tick(40)
+        screen.fill((255, 255, 255))
+
+        if on_intro:
+            if fade_counter == 3:
+                # Draw and animate the next view while fades out.
+                main_menu_view(screen, game_title_image, play_button_image,
+                          settings_button_image, quit_button_image,
+                          game_title_rect, play_button_rect,
+                          settings_button_rect, quit_button_rect)
+                animate_main_menu_view()
+            else:
+                intro_view(screen, logo_icon_image, logo_title_image,
+                           logo_icon_rect, logo_title_rect)
+            screen.blit(white_background, white_background_rect)
+            alpha_value += factor
+            if (alpha_value > 255 and not backwards) \
+            or (alpha_value < 0 and backwards):
+                backwards = not backwards
+                factor *= -1
+                fade_counter += 1
+            white_background.set_alpha(alpha_value)
+            white_background.fill((255, 255, 255))
+
+            if fade_counter == 4:
+                # Ends fade effect completely
+                on_intro = False
+        else:
+            main_menu_view(screen, game_title_image, play_button_image,
+                          settings_button_image, quit_button_image,
+                          game_title_rect, play_button_rect,
+                          settings_button_rect, quit_button_rect)
+            animate_main_menu_view()
+
+        clock.tick(60)
         pygame.display.update()
